f04d21796792b9283ddb8ec01766d6e4b3aebc49
Mike Hsu <mike.hsu@gmail.com>
2022-02-26 22:18:29 -0800

    draw painting area

--- src/painting.rs
+++ src/painting.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
+};
 use bevy_prototype_lyon::{prelude::*, shapes};
 
 use crate::game_state::GameState;
@@ -7,7 +10,11 @@ pub struct PaintingPlugin;
 impl Plugin for PaintingPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(ShapePlugin)
-            .add_system_set(SystemSet::on_enter(GameState::Painting).with_system(setup_brush))
+            .add_system_set(
+                SystemSet::on_enter(GameState::Painting)
+                    .with_system(setup_brush)
+                    .with_system(setup_painting_area),
+            )
             .add_system_set(SystemSet::on_update(GameState::Painting).with_system(track_cursor));
     }
 }
@@ -34,10 +41,36 @@ fn track_cursor(
     windows: Res<Windows>,
     mut brush: Query<&mut Transform, With<Paintbrush>>,
 ) {
-  let window = windows.get_primary().unwrap();
-  for position in cursor_pos.iter() {
-    let size = Vec2::new(window.width() as f32, window.height() as f32);
-    let mut t = brush.single_mut();
-    t.translation = (position.position - size / 2.0).extend(0.0);
-  }
+    let window = windows.get_primary().unwrap();
+    for position in cursor_pos.iter() {
+        let size = Vec2::new(window.width() as f32, window.height() as f32);
+        let mut t = brush.single_mut();
+        t.translation = (position.position - size / 2.0).extend(0.0);
+    }
+}
+
+#[derive(Component)]
+struct PaintingArea;
+
+fn setup_painting_area(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
+    let width = 600;
+    let height = 600;
+    let texture = images.add(Image::new_fill(
+        Extent3d {
+            width,
+            height,
+            ..Default::default()
+        },
+        TextureDimension::D2,
+        &[255, 255, 255, 255],
+        TextureFormat::Rgba8Unorm,
+    ));
+    commands.spawn().insert_bundle(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(width as f32, height as f32)),
+            ..Sprite::default()
+        },
+        texture,
+        ..SpriteBundle::default()
+    });
 }
